0fa77efcdd35d1eaebb2eb1b0914e2a74c2ed9cc
Daniel Sepulveda <danielgsepulvedaa@gmail.com>
2023-03-28 12:26:25 -0600

    add

--- src/server/api/routers/product.ts
+++ src/server/api/routers/product.ts
@@ -1,7 +1,11 @@
 import { type Product } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
+import { Document } from "langchain/document";
 import { OpenAIEmbeddings } from "langchain/embeddings";
+import { PineconeStore } from "langchain/vectorstores";
 import { z } from "zod";
 
+import { env } from "~/env.mjs";
 import {
   createTRPCRouter,
   protectedProcedure,
@@ -20,7 +24,7 @@ const addProductSchema = z.object({
   name: z.string(),
   url: z.string().url(),
   shortDesc: z.string(),
-  fullDesc: z.string().optional(),
+  fullDesc: z.string().optional().default(""),
   releaseYear: z.string().optional(),
   hasFreeTier: z.boolean().default(false),
 });
@@ -40,10 +44,47 @@ const addProduct = protectedProcedure
       },
     });
 
+    const productDescToEmbed = `${newProduct.name} - ${newProduct.shortDesc}`;
+    const productDoc = new Document({
+      pageContent: productDescToEmbed,
+    });
+
+    const pineconeIndex = ctx.pinecone.Index(env.PINECONE_INDEX);
+    const productStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings(),
+      { pineconeIndex, namespace: "description" },
+    );
+    await productStore.addDocuments([productDoc], [newProduct.id]);
+
     return newProduct;
+  });
+
+/* ----------------------------- DELETE PRODUCT ----------------------------- */
+
+const deleteProductSchema = z.object({
+  id: z.string(),
+});
+
+const deleteProduct = protectedProcedure
+  .meta({ openapi: { method: "DELETE", path: "/product" } })
+  .input(deleteProductSchema)
+  .mutation(async ({ ctx, input }) => {
+    const product = await ctx.prisma.product.findUnique({
+      where: { id: input.id },
+    });
+
+    if (!product) {
+      throw new TRPCError({ code: "NOT_FOUND", message: "Product not found" });
+    }
+
+    const pineconeIndex = ctx.pinecone.Index(env.PINECONE_INDEX);
+    await pineconeIndex.delete1({ ids: [product.id] });
+
+    return ctx.prisma.product.delete({ where: { id: input.id } });
   });
 
 export const productRouter = createTRPCRouter({
   list: listProducts,
   add: addProduct,
+  delete: deleteProduct,
 });
